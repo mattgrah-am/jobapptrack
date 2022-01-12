a387b4e32f734c4060a52c22fa6e94bf0d75532c
Matt Graham <iam@mattgrah.am>
2022-01-12 19:59:31 +1100

    update app.py to connect database and setup secret key

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 import psycopg2
 
 DB_URL = os.environ.get("DATABASE_URL", "dbname=jobapptrack")
+SECRET_KEY = os.environ.get("SECRET_KEY", "TEST_SECRET_KEY")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route('/')
