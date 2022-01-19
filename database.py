a6a90aaf30732ad0cd8117021e3651fafb6cd703
Matt Graham <iam@mattgrah.am>
2022-01-19 21:50:54 +1100

    config database

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ DB_URL = os.environ.get("DATABASE_URL", "dbname=jobapptrack")
 
 
 def sql_select(query, params):
-    conn = psycopg2.connect('dbname=DB_URL')
+    conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
     cur.execute(query, params)
     results = cur.fetchall()
@@ -15,7 +15,7 @@ def sql_select(query, params):
 
 
 def sql_write(query, params):
-    conn = psycopg2.connect('dbname=DB_URL')
+    conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
     cur.execute(query, params)
     conn.commit()
